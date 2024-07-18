2f8c1861752f20fe5a1be0e594559bb3e1855070
ifedayo <ifedayyo12@gmail.com>
2024-07-18 15:55:15 +0100

    push

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ export default function App() {
   return (
     <div className="app">
       <Header />
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question</p>
+      </main>
     </div>
   );
 }
