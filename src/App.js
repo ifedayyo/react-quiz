c65b33758b74985b6d3cb58f46a62507bf6c2bdb
ifedayo <ifedayyo12@gmail.com>
2024-07-18 16:47:39 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
+const initialState = {
+  questions: [],
+
+  //'loading', 'error', 'ready', 'active', 'finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 export default function App() {
+  //destructuring our state
+  const [{ question, status }, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch(`http://localhost:9000/questions`)
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz</h2>
+      <p> X question to test your react mastery</p>
+      <button> Let's start</button>
+    </div>
+  );
+}
