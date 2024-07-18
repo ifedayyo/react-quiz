29f640fd9537da88b6840ad12cf71ed7802524eb
ifedayo <ifedayyo12@gmail.com>
2024-07-18 13:35:06 +0100

    learning to use the useReducer hook which takes a function and a state. the function is defined outside the the hook. also makes use of action (which  include a type and a payload)

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,6 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
 }
 function DateCounter() {
   //const [count, setCount] = useState(0);
@@ -15,16 +18,19 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec", payload: -1 }); //we are going to create an "action", something we do when we work with
     // setCount((count) => count - 1);
     //setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "inc", payload: 1 });
     // setCount((count) => count + 1);
     //setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch(Number(e.target.value));
     //setCount(Number(e.target.value));
   };
 
