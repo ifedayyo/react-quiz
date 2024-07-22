4ea7d3d94b2b4c85d36a4aa79d675d3f81d90d93
ifedayo <ifedayyo12@gmail.com>
2024-07-22 14:56:14 +0100

    push

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
   //'loading', 'error', 'ready', 'active', 'finished
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -38,7 +39,10 @@ function reducer(state, action) {
 }
 export default function App() {
   //destructuring our state
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
   useEffect(function () {
@@ -57,7 +61,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- src/Question.js
+++ src/Question.js
@@ -1,3 +1,12 @@
-export default function Question() {
-  return <div>Question</div>;
+export default function Question({ question }) {
+  return (
+    <div>
+      <h4> {question.question}</h4>
+      <div>
+        {question.options.map((option) => (
+          <button className="btn btn-option">{option}</button>
+        ))}
+      </div>
+    </div>
+  );
 }
