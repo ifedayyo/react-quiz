4698edf2b9c87d41efea20ac66dc5c37b776edb8
ifedayo <ifedayyo12@gmail.com>
2024-07-22 15:31:40 +0100

    push

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,13 @@
-export default function Options({ question }) {
+export default function Options({ question, dispatch, answer }) {
   return (
     <div>
       <div>
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
+        {question.options.map((option, index) => (
+          <button
+            className="btn btn-option"
+            key={option}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
             {option}
           </button>
         ))}
