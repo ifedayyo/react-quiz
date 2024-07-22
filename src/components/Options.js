2b5c4b0770cafaa573d0f402bb05e799e3ea337c
ifedayo <ifedayyo12@gmail.com>
2024-07-22 15:43:36 +0100

    PUSH

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,13 +1,15 @@
 export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div>
       <div>
         {question.options.map((option, index) => (
           <button
             className={`btn btn-option ${index === answer ? `answer` : ""} ${
-              index === question.correctOption ? "correct" : "wrong"
+              hasAnswered ? "correct" : "wrong"
             }`}
             key={option}
+            disabled={answer !== null}
             onClick={() => dispatch({ type: "newAnswer", payload: index })}
           >
             {option}
