1284e7989f7b35a5a86de2e6e1631509cdd76c3a
ifedayo <ifedayyo12@gmail.com>
2024-07-23 14:17:49 +0100

    push

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -35,9 +36,14 @@ function reducer(state, action) {
         status: "active",
       };
     case "newAnswer":
+      const question = state.questions.at(state.index);
       return {
         ...state,
         answer: action.payload,
+        points:
+          action.paylod === question.correctOption
+            ? state.points + question.points
+            : state.points,
       };
     default:
       throw new Error("Action unknown");
