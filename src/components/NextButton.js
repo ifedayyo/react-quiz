9a5c9d12fe6175541ae4abca35573add87963129
ifedayo <ifedayyo12@gmail.com>
2024-07-23 14:48:36 +0100

    push

---
+++ src/components/NextButton.js
@@ -0,0 +1,14 @@
+export default function NextButton({ dispatch, answer }) {
+  if (answer === null) return null;
+
+  return (
+    <div>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
