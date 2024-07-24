3c25384296503034a595433b003a2ee349dfa514
ifedayo <ifedayyo12@gmail.com>
2024-07-24 13:49:54 +0100

    push

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,3 +1,11 @@
+import { useState, useEffect } from "react";
+
 export default function Timer() {
-  return <div>timer</div>;
+  const [counter, setCounter] = useState(60);
+
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  }, [counter]);
+
+  return <button className="btn btn-ui">{counter}</button>;
 }
